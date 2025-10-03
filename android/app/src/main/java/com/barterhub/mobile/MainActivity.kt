dee206d8d5b139e8d06c19d99d1851440793cbf6
masembe0757 <sendyj886@gmail.com>
2025-10-03 12:53:38 +0300

    fix : package name

--- android/app/src/main/java/com/barterhub/mobile/MainActivity.kt
+++ android/app/src/main/java/com/barterhub/mobile/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.barter_it_mobile
+package com.barterhub.mobile
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "Barter_it_mobile"
+  override fun getMainComponentName(): String = "BarterHub"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/barterhub/mobile/MainApplication.kt
+++ android/app/src/main/java/com/barterhub/mobile/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.barter_it_mobile
+package com.barterhub.mobile
 
 import android.app.Application
 import com.facebook.react.PackageList
